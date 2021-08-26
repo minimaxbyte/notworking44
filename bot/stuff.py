2b87cd0e59f9d14c7c9eb36ec3c47e84c8299f70
minimaxbyte <84379882+minimaxbyte@users.noreply.github.com>
2021-08-26 15:52:45 +0530

    Add files via upload

---
+++ bot/stuff.py
@@ -0,0 +1,64 @@
+#    This file is part of the Encoding distribution.
+#    Copyright (c) 2021
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3.
+#
+#    This program is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#    General Public License for more details.
+#
+# License can be found in <
+# https://github.com/> .
+
+from .worker import *
+
+
+async def up(event):
+    if not event.is_private:
+        return
+    stt = dt.now()
+    ed = dt.now()
+    v = ts(int((ed - uptime).seconds) * 1000)
+    ms = (ed - stt).microseconds / 1000
+    p = f"💥Pɪɴɢ = {ms}ms"
+    await event.reply(v + "\n" + p)
+
+
+async def start(event):
+    await event.reply(
+        f"Hi `{event.sender.first_name}`\nThis Encoder Bot Belongs To @OngoingAnimess",
+        buttons=[
+            [Button.inline("HELP", data="ihelp")],
+            [
+                Button.url("OngoingAnimess", url="https://t.me/OngoingAnimess"),
+            ],
+        ],
+    )
+
+
+async def help(event):
+    await event.reply(
+        "**🌐 This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot"
+    )
+
+
+async def ihelp(event):
+    await event.edit(
+        "**🌐 This Bot Is Not Public**\n\n✔️Only Authenticated Users Will Use This Bot",
+        buttons=[Button.inline("BACK", data="beck")],
+    )
+
+
+async def beck(event):
+    await event.edit(
+        f"Hi `{event.sender.first_name}`\nThis Encoder Bot Belongs To @OngoingAnimess",
+        buttons=[
+            [Button.inline("HELP", data="ihelp")],
+            [
+                Button.url("OngoingAnimess", url="https://t.me/OngoingAnimess"),
+            ],
+        ],
+    )
